966268ffb0f04c9923689953186da105f4a65b02
Valenttin Motter <valenttin@myscrumhalf.com>
2024-09-07 17:48:10 -0300

    feat: finalizando componente teamcard

--- src/components/TeamCard/styles.ts
+++ src/components/TeamCard/styles.ts
@@ -1,8 +1,26 @@
 import styled from "styled-components/native";
 import { MaterialIcons } from "@expo/vector-icons";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+  height: 96px;
+  border: 1px solid ${({ theme }) => theme.COLORS.GRAY_200};
+  padding: 32px 24px;
+  max-width: 100%;
+  margin-bottom: 12px;
+  border-radius: 6px;
+`;
 
-export const Icon = styled(MaterialIcons).attrs(({ theme }) => ({}))``;
+export const Icon = styled(MaterialIcons).attrs(({ theme }) => ({
+  size: 32,
+}))``;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_500};
+  font-size: 18px;
+  font-weight: 400;
+  text-transform: capitalize;
+`;
