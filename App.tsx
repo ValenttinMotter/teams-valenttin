421fda0bff23204e6265080dfdf9ecd510279139
Valenttin Motter <valenttin@myscrumhalf.com>
2024-10-28 21:50:31 -0300

    chore: implementado stack navigator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
-import { Teams } from "@screens/Teams";
 import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
 import { useFonts } from "expo-font";
 import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
-import { NewTeam } from "@screens/NewTeam";
-import { AddMembers } from "@screens/AddMembers";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <AddMembers /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
